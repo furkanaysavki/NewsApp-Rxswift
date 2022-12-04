d154e98b708b84ff56db38fbc28fd800c82614b2
furkanaysavki <furkanaysavki@gmail.com>
2022-12-04 21:05:17 +0300

    final touches

--- NewsApp-Rxswift/Models/Article.swift
+++ NewsApp-Rxswift/Models/Article.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct ArticleResponse : Decodable {
+    let articles : [Article]
+}
+
+struct Article: Decodable {
+    let title : String
+    let description : String?
+}
---
+++ Pods/Target Support Files/Pods-NewsApp-Rxswift/Pods-NewsApp-Rxswift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewsApp_Rxswift : NSObject
+@end
+@implementation PodsDummy_Pods_NewsApp_Rxswift
+@end
